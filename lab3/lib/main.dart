64d6eed771b9411ae6e6c31fff334ee283935d33
Evgeny Ermolov <evgenijermolov2005@gmail.com>
2026-03-13 19:03:33 +0300

    feat: rename app and title to 'Инкремент'

--- lab3/lib/main.dart
+++ lab3/lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Инкремент',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Инкремент'),
     );
   }
 }
