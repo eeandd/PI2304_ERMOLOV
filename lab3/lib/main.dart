57214764d0b404902b31f1e0bb3f57648874adbe
Evgeny Ermolov <evgenijermolov2005@gmail.com>
2026-03-13 19:06:23 +0300

    feat: change theme color to yellow

--- lab3/lib/main.dart
+++ lab3/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Инкремент',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.yellow)),
       home: const MyHomePage(title: 'Инкремент'),
     );
   }
