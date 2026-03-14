cebc85a5418832cda6e8dda04386b312433771cf
Evgeny Ermolov <evgenijermolov2005@gmail.com>
2026-03-15 00:31:13 +0300

    feat: add text and apply styling

--- lab4/lib/main.dart
+++ lab4/lib/main.dart
@@ -39,7 +39,43 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(children: [Image.asset('assets/20.webp')]),
+        child: Column(
+          children: [
+            Image.asset('assets/20.webp'),
+            Padding(
+              padding: EdgeInsets.all(32),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Общежитие №20',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              'Краснодар, ул. Калинина 13к20',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
