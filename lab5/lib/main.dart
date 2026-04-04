fc635d82dee5b5ddb4b0ca18f51dcd5a2117e505
Evgeny Ermolov <evgenijermolov2005@gmail.com>
2026-04-04 03:38:13 +0300

    feat: implement infinity math list

--- lab5/lib/main.dart
+++ lab5/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'simple_list.dart';
 import 'infinity_list.dart';
+import 'infinity_math_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.green,
         title: Text(widget.title, style: TextStyle(color: Colors.white)),
       ),
-      body: InfinityList(),
+      body: InfinityMathList(),
     );
   }
 }
