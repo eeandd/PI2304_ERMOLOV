6449505df75a710cc8eda6e983c105c27a7f41b7
Evgeny Ermolov <evgenijermolov2005@gmail.com>
2026-04-05 18:10:54 +0300

    feat: implement second screen UI

--- lab7/lib/main.dart
+++ lab7/lib/main.dart
@@ -8,8 +8,34 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('Возвращение значения')),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          },
           child: const Text('Приступить к выбору...'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Выберите любой вариант')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Да')),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: const Text('Нет')),
+          ],
         ),
       ),
     );
@@ -20,7 +46,10 @@ void main() {
   runApp(
     MaterialApp(
       initialRoute: '/',
-      routes: {'/': (BuildContext context) => const MainScreen()},
+      routes: {
+        '/': (BuildContext context) => const MainScreen(),
+        '/second': (BuildContext context) => const SecondScreen(),
+      },
     ),
   );
 }
